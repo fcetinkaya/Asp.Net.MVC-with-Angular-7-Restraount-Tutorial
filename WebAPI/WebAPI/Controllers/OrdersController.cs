cbe8a0340b79b21f193810ebefd3765d631e435d
fatih.cetinkaya <fatih.cetinkaya@FTH>
2019-02-08 11:14:00 +0300

    Soruce Code
    Share

---
+++ WebAPI/WebAPI/Controllers/OrdersController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Orders
+        public Object GetOrders()
+        {
+            var result = (from a in db.Orders
+                          join b in db.Customers on a.CustomerID equals b.CustomerID
+                          select new
+                          {
+                              a.OrderID,
+                              a.OrderNo,
+                              Customer = b.Name,
+                              a.PMethod,
+                              a.GTotal
+                          }).ToList();
+
+
+            return result;
+        }
+
+        // GET: api/Orders/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetOrder(long id)
+        {
+            var order = (from a in db.Orders
+                         where a.OrderID == id
+                         select new
+                         {
+                             a.OrderID,
+                             a.OrderNo,
+                             a.CustomerID,
+                             a.PMethod,
+                             a.GTotal,
+                             a.DeleteOrderItemIDs
+                         }).FirstOrDefault();
+
+            var orderDetails = (from a in db.OrderItems
+                                join b in db.Items on a.ItemID equals b.ItemID
+                                where a.OrderID == id
+                                select new
+                                {
+                                    a.OrderID,
+                                    a.OrderItemID,
+                                    a.ItemID,
+                                    ItemName = b.Name,
+                                    b.Price,
+                                    a.Quantity,
+                                    Total = a.Quantity * b.Price
+                                }).ToList();
+            return Ok(new { order, orderDetails });
+        }
+
+        // PUT: api/Orders/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutOrder(long id, Order order)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != order.OrderID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Orders
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult PostOrder(Order order)
+        {
+            try
+            {
+                if (order.OrderID == 0)
+                {
+                    //Order table
+                    db.Orders.Add(order);
+                }
+                else
+                {
+                    db.Entry(order).State = EntityState.Modified;
+                }
+
+
+                // OrderItems table
+                foreach (var item in order.OrderItems)
+                {
+                    if (item.OrderItemID == 0)
+                        db.OrderItems.Add(item);
+                    else
+                        db.Entry(item).State = EntityState.Modified;
+                }
+
+                // Delete for OrderItems
+                foreach (var item in order.DeleteOrderItemIDs.Split(',').Where(x => x != ""))
+                {
+                    OrderItem x = db.OrderItems.Find(Convert.ToInt64(item));
+                    db.OrderItems.Remove(x);
+                }
+                db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        // DELETE: api/Orders/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult DeleteOrder(long id)
+        {
+            Order order = db.Orders.Include(y => y.OrderItems)
+                .SingleOrDefault(x => x.OrderID == id);
+
+            foreach (var item in order.OrderItems.ToList())
+            {
+                db.OrderItems.Remove(item);
+            }
+
+            db.Orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(long id)
+        {
+            return db.Orders.Count(e => e.OrderID == id) > 0;
+        }
+    }
+}
